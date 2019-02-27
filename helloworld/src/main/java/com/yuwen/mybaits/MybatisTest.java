d28032d092c97998ab43792ee9bc9925c7b85772
yuxf <yuxinfang@ktwlrj.com>
2019-02-27 10:00:14 +0800

    test

--- helloworld/src/main/java/com/yuwen/mybaits/MybatisTest.java
+++ helloworld/src/main/java/com/yuwen/mybaits/MybatisTest.java
@@ -15,78 +15,90 @@ public class MybatisTest {
 
 	/**
 	 * 根据id查询学生
+	 * 
 	 * @throws IOException
 	 */
 	@Test
 	public void GetStudentById() throws IOException {
-		
-		SqlSession sqlSession=SqlSessionFactoryUtils.getSqlSessionFactory().openSession();
-		Object obj=sqlSession.selectOne("student.getStudentById",1);
+
+		SqlSession sqlSession = SqlSessionFactoryUtils.getSqlSessionFactory().openSession();
+		Object obj = sqlSession.selectOne("student.getStudentById", 1);
 		System.out.println(obj);
- 
+
 		sqlSession.close();
 	}
+
 	/**
 	 * 根据条件模糊查询学生
+	 * 
 	 * @throws IOException
 	 */
 	@Test
 	public void GetStudentByName() throws IOException {
-		
-		SqlSession sqlSession=SqlSessionFactoryUtils.getSqlSessionFactory().openSession();
-		List<StudentModel> obj=sqlSession.selectList("student.getStudentByName","小");
+
+		SqlSession sqlSession = SqlSessionFactoryUtils.getSqlSessionFactory().openSession();
+		List<StudentModel> obj = sqlSession.selectList("student.getStudentByName", "小");
 		System.out.println(obj);
- 
+
 		sqlSession.close();
 	}
-	
-	
+
 	/**
 	 * 插入学生，返回自增id
+	 * 
 	 * @throws IOException
 	 */
 	@Test
 	public void AddStuent() throws IOException {
-	
-		StudentModel stu=new StudentModel();
-		
+
+		StudentModel stu = new StudentModel();
+
 		stu.setName("张飞");
 		stu.setAge(18);
 		stu.setGender(true);
 		stu.setCreate_time(new Date());
-		
-		SqlSession sqlSession=SqlSessionFactoryUtils.getSqlSessionFactory().openSession();
-		sqlSession.insert("student.addStudent",stu);
+
+		SqlSession sqlSession = SqlSessionFactoryUtils.getSqlSessionFactory().openSession();
+		sqlSession.insert("student.addStudent", stu);
 		System.out.println(stu);
-		sqlSession.commit();//一定要提交才会保存到数据库
+		sqlSession.commit();// 一定要提交才会保存到数据库
 		sqlSession.close();
-		
+
 	}
-	
-	
+
+	/**
+	 * 插入学生，返回自增id
+	 * 
+	 * @throws IOException
+	 */
+	@Test
+	public void updateStuent() throws IOException {
+
+		StudentModel stu = new StudentModel();
+		stu.setStudent_id(8);
+		stu.setName("张飞fei");
+		SqlSession sqlSession = SqlSessionFactoryUtils.getSqlSessionFactory().openSession();
+		sqlSession.update("student.updateStudent", stu);
+
+		sqlSession.commit();// 一定要提交才会保存到数据库
+		sqlSession.close();
+
+	}
+
 	/**
 	 * 删除学生，返回自增id
+	 * 
 	 * @throws IOException
 	 */
 	@Test
 	public void RemoveStuent() throws IOException {
 
+		SqlSession sqlSession = SqlSessionFactoryUtils.getSqlSessionFactory().openSession(true);//这里设置为true，则会自动提交
+		sqlSession.delete("student.removeStudentById", 7);
+
 		
-		SqlSession sqlSession=SqlSessionFactoryUtils.getSqlSessionFactory().openSession();
-		sqlSession.delete("student.removeStudentById",7);
-		
-		sqlSession.commit();//一定要提交才会保存到数据库
 		sqlSession.close();
-		
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
