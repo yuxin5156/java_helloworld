cc9e882ddf6865645e7a60ca8d668d68fdc7642d
yuxf <yuxinfang@ktwlrj.com>
2019-03-04 14:31:51 +0800

    test

---
+++ helloworld/src/main/java/com/yuwen/utils/IdCardUtils.java
@@ -0,0 +1,70 @@
+package com.yuwen.utils;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * 证件号码解析帮助类
+ * 
+ * @author yxf
+ *
+ */
+public class IdCardUtils {
+
+	/**
+	 * 获取性别
+	 * 
+	 * @param idCard 证件号码
+	 * @return
+	 */
+	public static boolean getSex(String idCard) {
+		String sexCode = idCard.substring(16, 17);
+		return Integer.parseInt(sexCode) % 2 == 1;
+	}
+
+	/**
+	 * 获取年龄
+	 * 
+	 * @param idCard 证件号码
+	 * @return
+	 */
+	public static int getAge(String idCard) {
+		String yearStr = idCard.substring(6, 10);
+		int year = Integer.parseInt(yearStr);
+		int nowYear = Calendar.getInstance().get(Calendar.YEAR);
+		return nowYear - year;
+	}
+
+	/**
+	 * 获取出生日期
+	 * 
+	 * @param idCard 证件号码
+	 * @return
+	 */
+	public static Calendar getBirthDate(String idCard) {
+		Calendar cal = Calendar.getInstance();
+		// 获取年
+		String yearStr = idCard.substring(6, 10);
+		// 获取月
+		String monthStr = idCard.substring(10, 12);
+		// 获取日
+		String dayStr = idCard.substring(12, 14);
+
+		cal.set(Integer.parseInt(yearStr), Integer.parseInt(monthStr)-1, Integer.parseInt(dayStr));
+		return cal;
+	}
+	
+
+	/**
+	 *  验证证件号码是否正确
+	 * @param idCard
+	 * @return
+	 * @throws Exception 
+	 */
+	public static boolean validateIdCard(String idCard) throws Exception
+	{
+			throw new Exception("dd");
+		
+	}
+
+}
---
+++ helloworld/src/main/java/com/yuwen/utils/test/IdCardUtilsTest.java
@@ -0,0 +1,35 @@
+package com.yuwen.utils.test;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import org.junit.Test;
+
+import com.yuwen.utils.IdCardUtils;
+
+public class IdCardUtilsTest {
+
+	public static final String ID_CARD = "520115201709132047";
+
+	@Test
+	public void getSex() {
+
+		boolean sex = IdCardUtils.getSex(ID_CARD);
+		System.out.println(sex);
+	}
+
+	@Test
+	public void getAge() {
+		int result = IdCardUtils.getAge(ID_CARD);
+		System.out.println(result + "岁");
+	}
+
+	@Test
+	public void getBirthDate() {
+		Calendar result = IdCardUtils.getBirthDate(ID_CARD);
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		String ste = sdf.format(result.getTime());
+		System.out.println(ste);
+	}
+
+}
