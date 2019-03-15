4796c91a4df146d1d0d900932a66d098a433642e
yuxf <yuxinfang@ktwlrj.com>
2019-03-15 15:44:40 +0800

    序列化帮助类

--- helloworld/src/main/java/com/yuwen/utils/EnumUtils.java
+++ helloworld/src/main/java/com/yuwen/utils/EnumUtils.java
@@ -1,11 +1,119 @@
 package com.yuwen.utils;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * 枚举帮助类
+ * 枚举处理器https://www.cnblogs.com/draem0507/p/4110987.html
+ * 
  * @author yxf
- *
+ * 
  */
 public class EnumUtils {
-
 	
+
+	public EnumUtils() {
+
+		
+	}
+	/**
+	 * 初始化配置信息
+	 */
+	protected EnumConfig InitConfig()
+	{
+		EnumConfig config=new EnumConfig();
+		return config;
+	}
+	
+	/**
+	 * 获取包下所有类
+	 */
+	public static List<String> getClasses(String packageName)
+	{
+		//获取包绝对路径
+		String packageAbsPath=ClassLoader.getSystemResource("").getPath()+packageName.replace('.', '\\');
+		//获取包下所有类
+		List<String> classes=new ArrayList<String>();
+		File file=new File(packageAbsPath);
+		
+		File[] listFiles = file.listFiles();
+		for (File childFile : listFiles) {
+			if(!childFile.isDirectory())
+			{
+				
+			}
+		}
+		
+		return null;
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println(ClassLoader.getSystemResource("").getPath());
+	}
+	
+	
+	
+
+}
+
+/**
+ * 枚举配置
+ * 
+ * @author Administrator
+ *
+ */
+class EnumConfig {
+	/**
+	 * 包全名称
+	 */
+	private String packageName;
+	/**
+	 * 枚举接口类全路径
+	 */
+	private String ienumName;
+
+	/**
+	 * 获取值的方法名
+	 */
+	private String getValueMethod;
+
+	/**
+	 * 获取描述的方法名
+	 */
+	private String getDescriptionMethod;
+
+	public String getPackageName() {
+		return packageName;
+	}
+
+	public void setPackageName(String packageName) {
+		this.packageName = packageName;
+	}
+
+	public String getIenumName() {
+		return ienumName;
+	}
+
+	public void setIenumName(String ienumName) {
+		this.ienumName = ienumName;
+	}
+
+	public String getGetValueMethod() {
+		return getValueMethod;
+	}
+
+	public void setGetValueMethod(String getValueMethod) {
+		this.getValueMethod = getValueMethod;
+	}
+
+	public String getGetDescriptionMethod() {
+		return getDescriptionMethod;
+	}
+
+	public void setGetDescriptionMethod(String getDescriptionMethod) {
+		this.getDescriptionMethod = getDescriptionMethod;
+	}
+
 }
--- helloworld/src/main/java/com/yuwen/utils/SerializeUtils.java
+++ helloworld/src/main/java/com/yuwen/utils/SerializeUtils.java
@@ -1,16 +1,77 @@
 package com.yuwen.utils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SerializeUtils implements Serializable{
+public class SerializeUtils {
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException
+	{
+		List<Sperson> list=new ArrayList<Sperson>();
+		for (int i = 0; i < 10; i++) {
+			Sperson p=new Sperson();
+			p.setName("yuxinf"+i);
+			p.setAge(16+i);
+			p.setGender(i%2==0);
+			list.add(p);
+		}
+		
+		//序列化到文件
+		ObjectOutputStream os=new ObjectOutputStream(new FileOutputStream(new File("d:/demo/jserialize.txt")));
+		os.writeObject(list);
+		os.close();
+		System.out.println("序列化成功");
+		//反序列化
+		ObjectInputStream ois=new ObjectInputStream(new FileInputStream(new File("d:/demo/jserialize.txt")));
+		List<Sperson> dlist= (List<Sperson>)ois.readObject();
+		
+		System.out.println(dlist);
+		
+		
+		
+	}
+	
+}
 
-	/**
-	 * 
-	 */
+class Sperson implements Serializable
+{
+	
 	private static final long serialVersionUID = 1L;
+	private String name;
+	private int age;
+	private boolean gender;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public boolean isGender() {
+		return gender;
+	}
+	public void setGender(boolean gender) {
+		this.gender = gender;
+	}
+	@Override
+	public String toString() {
+		return "Sperson [name=" + name + ", age=" + age + ", gender=" + gender + "]";
+	}
 	
 	
-
 	
 	
 }
